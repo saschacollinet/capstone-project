cd0a51d9c2ccdd3dc5635bfa68c3ff04edd5347c
Sascha Collinet <collinet.sascha@gmail.com>
2021-10-15 16:04:23 +0200

    Add logo.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components/macro'
+import logo from '../../assets/images/logo.svg'
 
 export default function Header() {
   return (
     <Wrapper>
-      <h1>Allaround Family</h1>
+      <Logo src={logo} alt="Allaround Family logo" />
     </Wrapper>
   )
 }
@@ -12,9 +13,13 @@ const Wrapper = styled.header`
   background-color: rgba(255, 255, 255, 0.5);
   backdrop-filter: blur(10px);
   width: 100%;
-  padding: 1.3rem 0;
-  text-align: center;
+  /* padding: 1.3rem 0; */
   z-index: 2;
   position: fixed;
   top: 0;
+`
+
+const Logo = styled.img`
+  height: 80px;
+  padding: 0.5rem;
 `
