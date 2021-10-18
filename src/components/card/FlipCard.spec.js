44affd468ffb5627d77c7236adf38e4ce2333a45
Sascha Collinet <collinet.sascha@gmail.com>
2021-10-18 19:29:26 +0200

    Grop tests a bit better.

--- src/components/card/FlipCard.spec.js
+++ src/components/card/FlipCard.spec.js
@@ -29,11 +29,9 @@ describe('FlipCard', () => {
     )
 
     const title = screen.getByRole('heading')
-
     expect(title).toBeInTheDocument()
 
     const link = screen.getByRole('link')
-
     expect(link).toBeInTheDocument()
   })
 
@@ -55,53 +53,41 @@ describe('FlipCard', () => {
     const titleAndNameContent = screen.getAllByText(
       'Natur- und Tierpark Brüggen'
     )
-
     expect(titleAndNameContent).toHaveLength(2)
 
     const nameTerm = screen.getByText('Name:')
-
     expect(nameTerm).toBeInTheDocument()
 
     const subtitle = screen.getByText('Brüggen')
-
     expect(subtitle).toBeInTheDocument()
 
     const descrioptionTerm = screen.getByText('Description:')
-
     expect(descrioptionTerm).toBeInTheDocument()
 
     const descriptionContent = screen.getByText(
       'Der Natur- und Tierpark Brüggen ist ein Zoo mit umfangreichem Spiel- und Freizeitbereich!'
     )
-
     expect(descriptionContent).toBeInTheDocument()
 
     const addressTerm = screen.getByText('Address:')
-
     expect(addressTerm).toBeInTheDocument()
 
     const openingHoursTerm = screen.getByText('Opening Hours:')
-
     expect(openingHoursTerm).toBeInTheDocument()
 
     const openingHoursContent = screen.getByText('09:00 - 18:00')
-
     expect(openingHoursContent).toBeInTheDocument()
 
     const websiteTerm = screen.getByText('Website:')
-
     expect(websiteTerm).toBeInTheDocument()
 
     const websiteContent = screen.getByText('Click here!')
-
     expect(websiteContent).toBeInTheDocument()
 
     const prizesTerm = screen.getByText('Prizes:')
-
     expect(prizesTerm).toBeInTheDocument()
 
     const prizesContent = screen.getByText('Please check their website.')
-
     expect(prizesContent).toBeInTheDocument()
   })
 })
