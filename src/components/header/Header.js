cb69379d56e3b908ec8fb6fc8cfa62b2cf544e21
Sascha Collinet <collinet.sascha@gmail.com>
2021-10-22 19:54:45 +0200

    Add NavLink to Logo.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components/macro'
 import logo from '../../assets/images/logo.svg'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
     <Wrapper>
       <h1>
-        <Logo src={logo} alt="Allaround Family" />
+        <StyledLink to="/">
+          <Logo src={logo} alt="Allaround Family" />
+        </StyledLink>
       </h1>
     </Wrapper>
   )
@@ -18,6 +21,12 @@ const Wrapper = styled.header`
   z-index: 9;
   position: fixed;
   top: 0;
+`
+
+const StyledLink = styled(NavLink)`
+  &:focus {
+    outline: none;
+  }
 `
 
 const Logo = styled.img`
