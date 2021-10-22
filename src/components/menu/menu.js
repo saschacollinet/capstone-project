62615d7842381cef83c7d8f9d91d22bfe7d0acf3
Sascha Collinet <collinet.sascha@gmail.com>
2021-10-22 19:55:21 +0200

    Add NavLinks and onclick.

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components/macro'
+import { NavLink } from 'react-router-dom'
 
-export default function Menu({ open }) {
+export default function Menu({ open, setOpen }) {
   return (
     <StyledMenu open={open}>
-      <a href="/">Home</a>
-      <a href="/">Activity List</a>
-      <a href="/">Add Activity</a>
+      <StyledLink to="/" open={open} onClick={() => setOpen(!open)}>
+        Home
+      </StyledLink>
+      <StyledLink to="/list" open={open} onClick={() => setOpen(!open)}>
+        Activity List
+      </StyledLink>
+      <StyledLink to="/create" open={open} onClick={() => setOpen(!open)}>
+        Add Activity
+      </StyledLink>
     </StyledMenu>
   )
 }
@@ -28,21 +35,21 @@ const StyledMenu = styled.nav`
   @media (max-width: ${({ theme }) => theme.mobile}) {
     width: 100%;
   }
+`
 
-  a {
-    font-size: 1.2rem;
-    text-transform: uppercase;
-    padding: 2rem 0;
-    font-weight: 700;
-    letter-spacing: 0.5rem;
-    color: ${({ theme }) => theme.primaryDark};
+const StyledLink = styled(NavLink)`
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  padding: 2rem 0;
+  font-weight: 700;
+  letter-spacing: 0.5rem;
+  color: ${({ theme }) => theme.primaryDark};
 
-    @media (max-width: ${({ theme }) => theme.mobile}) {
-      text-align: center;
-    }
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    text-align: center;
+  }
 
-    &:hover {
-      color: ${({ theme }) => theme.primaryHover};
-    }
+  &:hover {
+    color: ${({ theme }) => theme.primaryHover};
   }
 `
